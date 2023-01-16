3118e35aee2885208290f5d9ef04cd8a881c13bc
Chukwudi Wisdom <55758868+override-bot@users.noreply.github.com>
2023-01-16 07:59:50 +0100

    Update house_model.dart

---
+++ ownorent-mobile/lib/core/models/house_model.dart
@@ -0,0 +1,39 @@
+class House {
+  String? id;
+  String? type; //rent, sale
+  String? address;
+  String? description;
+  List<String>? images; //list of urls
+  String? videoTour; //url
+  String? bedroomNumber;
+  String? bathroomNumber;
+  double? locationLat;
+  double? locationLong;
+  String? accomodationType; //bungalow, duplex, flat
+  String? ownersId; //
+  House(
+      {this.accomodationType,
+      this.address,
+      this.bathroomNumber,
+      this.bedroomNumber,
+      this.description,
+      this.id,
+      this.images,
+      this.locationLat,
+      this.locationLong,
+      this.ownersId,
+      this.type,
+      this.videoTour});
+  House.fromMap(Map<String, dynamic> snapshot, this.id)
+      : accomodationType = snapshot['accomodationType'],
+        address = snapshot['address'],
+        bathroomNumber = snapshot['bathroomNumber'],
+        bedroomNumber = snapshot['bedroomNumber'],
+        description = snapshot['description'],
+        images = snapshot['images'],
+        locationLat = snapshot['locationLat'],
+        locationLong = snapshot['locationLong'],
+        ownersId = snapshot['ownersId'],
+        type = snapshot['type'],
+        videoTour = snapshot['videoTour'];
+}
