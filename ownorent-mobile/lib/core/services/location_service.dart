c7bf74b2369b8e938ac9f09f9f3f30a6f8a35d9b
Chukwudi Wisdom <55758868+override-bot@users.noreply.github.com>
2023-01-22 15:34:10 +0100

    location service

---
+++ ownorent-mobile/lib/core/services/location_service.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import "package:geocoding/geocoding.dart";
+
+class LocationService extends ChangeNotifier {
+  LatLng? _userCoordinates;
+  String? _location;
+  LatLng? get userCoordinates => _userCoordinates;
+  String? get location => _location;
+
+  Future<Position> getPosition() async {
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.best,
+    );
+    return position;
+  }
+
+  setCoordinates(coordinates) {
+    _userCoordinates = coordinates;
+  }
+
+  setAddress(address) {
+    _location = address;
+    notifyListeners();
+  }
+
+  getAddress(LatLng position) async {
+    List<Placemark> addresses =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+    var first = addresses.first;
+    return first.locality;
+  }
+}
